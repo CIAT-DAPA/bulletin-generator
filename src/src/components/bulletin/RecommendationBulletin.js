93113f93c3885c87d23c6d2c766ceecf0b409f1d
Sebastian Lopez <sebastian.lopez_ast@uao.edu.co>
2025-04-10 08:26:52 -0500

    bulletin recommendation started

--- src/src/components/bulletin/RecommendationBulletin.js
+++ src/src/components/bulletin/RecommendationBulletin.js
@@ -1,11 +1,13 @@
 import React, { useContext } from "react";
 import { FormDataContext } from "../../context/FormDataContext";
 import lluviaIco from "../../assets/lluviaIco.png";
+import recommendationIco from "../../assets/recommendationIco.png";
+import familyImg from "../../assets/family.png";
 
 function RecommendationBulletin() {
   const { formData } = useContext(FormDataContext);
   return (
-    <div className="d-flex flex-column justify-content-start h-100 p-2 gap-0">
+    <div className="d-flex flex-column justify-content-start h-100 p-2 gap-2">
       {/* Sección Superior */}
       <div className="text-center d-flex justify-content-around align-items-center bg-dark-transparent text-white rounded-1 px-4 py-2">
         <div>
@@ -24,6 +26,35 @@ function RecommendationBulletin() {
         </div>
       </div>
       {/* Sección Recomendaciones */}
+      <div className="text-center">
+        <div className="d-flex justify-content-center align-items-center bg-dark-transparent text-white rounded-1 gap-2 py-1">
+          <img
+            src={recommendationIco}
+            alt="Lluvia"
+            style={{ height: "24px" }}
+          />
+          <span className="fw-normal">Recomendaciones</span>
+        </div>
+        <img src={familyImg} alt="Familia" className="img-fluid" />
+        <div className="d-flex justify-content-around align-items-center bg-dark-transparent-content rounded-1">
+          <ul>
+            {formData.recommendations.length === 0 ? (
+              <p className="text-muted">No hay recomendaciones</p>
+            ) : (
+              formData.recommendations.map((rec, index) => (
+                <li
+                  key={index}
+                  className="list-group-item d-flex justify-content-between align-items-center"
+                >
+                  <span>
+                    <strong>Recomendación:</strong> {rec.recommendation}
+                  </span>
+                </li>
+              ))
+            )}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
