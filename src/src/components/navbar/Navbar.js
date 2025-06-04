e5d5553c8f44ad07e9a6af4910ad0227fb41f6f1
Minotriz02 <sebastian.lopez_Ast@uao.edu.com>
2025-06-04 14:24:44 -0500

    version b  hided

--- src/src/components/navbar/Navbar.js
+++ src/src/components/navbar/Navbar.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { Navbar as BSNavbar, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function Navbar({ appName }) {
   return (
     <BSNavbar bg="dark" variant="dark">
       <Container>
-        <BSNavbar.Brand href="#home">{appName}</BSNavbar.Brand>
+        <Link className="navbar-brand d-flex align-items-center" to="/bulletin-generator">
+          {appName}
+        </Link>
+        <Link className="nav-link" to="/b">
+          Version B
+        </Link>
       </Container>
     </BSNavbar>
   );
